1b5f4c559dc5c530262be181b057de4bb211fb2a
Nischal Bhattarai <nishchalb21@gmail.com>
2024-08-18 17:50:07 +0545

    last hai last

--- server/index.js
+++ server/index.js
@@ -10,9 +10,21 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+const allowedOrigins = ['http://localhost:5173', 'https://mern-notes-app-rose.vercel.app'];
+
+// Configure CORS
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Routes
